7f2dc6edc624e1963da0ba040f2dabf39a26b309
Pathum Disanayaka <kumadisanayaka18@gmail.com>
2026-07-29 15:42:18 +0530

    feat: implement task persistence with localStorage and improve task input handling

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-let tasks = [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
 let taskInput = document.getElementById("taskInput");
 let addbtn = document.getElementById("addbtn");
 let taskList = document.getElementById("taskList");
 
+
 addbtn.addEventListener("click", function () {
+
+    let taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        return;
+    }
+
     let task = {
-        text: taskInput.value,
+        text: taskText,
         completed: false
     };
 
     tasks.push(task)
 
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
     displayTasks();
+
+    taskInput.value = "";
 })
 
 function displayTasks() {
@@ -25,3 +37,4 @@ function displayTasks() {
 
     });
 }
+
